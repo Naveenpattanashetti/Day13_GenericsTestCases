7587bc5f9c60f0644134cf2406db1b8abb3d6254
Naveen Pattanashetti <pattanashettinaveen.com>
2021-10-10 23:19:05 +0530

    added:Welcome massge to user

---
+++ src/com/bridgelabz/generictestcases/Maximum.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.generictestcases;
+
+public class Maximum {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome Message for Generic Test Cases");
+
+	}
+
+}
